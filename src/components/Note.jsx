308dc3cacd5a572eb4371a30b86b6f4db53c7a60
KK1528 <2020UEC1528@MNIT.AC.IN>
2024-02-20 23:17:17 +0530

    efnrjkwebrjkewb

---
+++ src/components/Note.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Note(props){
+
+    function handleclick(event){
+        props.deleteNote(props.id);
+    }
+
+    return (
+    <div className="note">
+        <h2>{props.title}</h2>
+        <p>{props.content}</p>
+        <button onClick = {handleclick} >DELETE</button>
+    </div>
+    );
+}
+
+export default Note;
